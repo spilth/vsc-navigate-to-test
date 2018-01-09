32ff0b2e696880dca44f5718417b952995217ca7
Brian Kelly <polymonic@gmail.com>
2018-01-09 10:13:57 -0500

    Show existing editor if file is already open. Fixes #1

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,7 @@
 'use strict';
 
 import * as vscode from 'vscode';
+import { TextEditor, ViewColumn } from 'vscode';
 
 export function activate(context: vscode.ExtensionContext) {
     let disposable = vscode.commands.registerCommand('extension.navigateToTest', () => {
@@ -9,16 +10,55 @@ export function activate(context: vscode.ExtensionContext) {
         if (currentFile.endsWith('.ts')) {
             if (currentFile.includes('.spec')) {
                 let implementationFile = currentFile.replace(".spec.ts", ".ts")
+                
+                let alreadyOpen = false;
+                let exisitingEditor: TextEditor = null;
+                let existingColumn : ViewColumn = null;
 
-                vscode.workspace.openTextDocument(implementationFile).then((textDocument)=> {
-                    vscode.window.showTextDocument(textDocument, vscode.ViewColumn.One);                    
-                });
+                vscode.window.visibleTextEditors.forEach((editor) => {
+                    if (editor.document.fileName == implementationFile) {
+                        console.log(editor.document.fileName);
+                        alreadyOpen = true;
+                        exisitingEditor = editor;
+                        existingColumn = editor.viewColumn;
+                    }
+                })
+
+                if (!alreadyOpen) {
+                    vscode.workspace.openTextDocument(implementationFile).then((textDocument)=> {
+                        vscode.window.showTextDocument(textDocument, vscode.ViewColumn.One);                    
+                    });
+                } else {
+                    vscode.workspace.openTextDocument(implementationFile).then((textDocument)=> {
+                        vscode.window.showTextDocument(textDocument, existingColumn);                    
+                    })
+                }
             } else {
                 let testFile = currentFile.replace(".ts", ".spec.ts");
 
-                vscode.workspace.openTextDocument(testFile).then((textDocument)=> {
-                    vscode.window.showTextDocument(textDocument, vscode.ViewColumn.One);                    
+                let alreadyOpen = false;
+                let exisitingEditor: TextEditor = null;
+                let existingColumn : ViewColumn = null;
+
+                vscode.window.visibleTextEditors.forEach((editor) => {
+                    if (editor.document.fileName == testFile) {
+                        console.log(editor.document.fileName);
+
+                        alreadyOpen = true;
+                        exisitingEditor = editor;
+                        existingColumn = editor.viewColumn;
+                    }
                 })
+
+                if (!alreadyOpen) {
+                    vscode.workspace.openTextDocument(testFile).then((textDocument)=> {
+                        vscode.window.showTextDocument(textDocument, vscode.ViewColumn.One);                    
+                    })
+                } else {
+                    vscode.workspace.openTextDocument(testFile).then((textDocument)=> {
+                        vscode.window.showTextDocument(textDocument, existingColumn);                    
+                    })
+                }
             }
         }
     });
